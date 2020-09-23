444489ae4f3dab49eead73ba97333f116226f2c6
lukeg <lukeg@DESKTOP-90M9D7K>
2020-09-23 16:05:15 +1200

    Add project files.

---
+++ HealthClinic_MVC/Models/Patient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HealthClinic_MVC.Models
+{
+    //stores the patients details
+    public class Patient
+    {
+        //primary key
+        public int Id { get; set; }
+
+        //holds patients name
+        public string Name { get; set; }
+
+        //holds patients mobile number
+        [Display(Name = "Mobile Number")]
+        public string MobileNumber { get; set; }
+    }
+}
